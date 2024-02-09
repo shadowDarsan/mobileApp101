84893db1d85eed71b6ad0d581fa8697dc661ffec
ShadowDarsan <panerusamrat@gmail.com>
2024-02-09 11:45:57 +0545

    added gesture and can drag emojis

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import IconButton from './components/IconButton'
 import EmojiPicker from './components/EmojiPicker';
 import EmojiList from './components/EmojiList';
 import EmojiSticker from './components/EmojiSticker';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 const PlaceHolderImage = require('./assets/images/background-image.png')
 
@@ -51,7 +52,8 @@ export default function App() {
 
 
   return (
-    <View style={styles.container}>
+
+    <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer placeHolderImageSource={PlaceHolderImage} selectedImage={selectedImage} />
         {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />}
@@ -80,7 +82,7 @@ export default function App() {
         <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
       </EmojiPicker>
       <StatusBar style='auto' />
-    </View>
+    </GestureHandlerRootView>
   );
 }
 
